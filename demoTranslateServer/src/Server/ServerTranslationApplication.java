853884eaf207a9007b40418836e2ad20ac152dd5
Angel <qifang_2012@outlook.com>
2021-04-09 16:16:08 +0800

    Initial Commit

---
+++ demoTranslateServer/src/Server/ServerTranslationApplication.java
@@ -0,0 +1,31 @@
+package Server;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerTranslationApplication{
+public static void main(String[] args)throws IOException{
+	ServerSocket serverSocket = null;
+	try{
+
+int portNo=4228;
+serverSocket=new ServerSocket(portNo);
+
+String text1="Good Afternoon";
+System.out.println("Waiting for request");
+
+	while(true){
+		Socket clientSocket = serverSocket.accept();
+		DataOutputStream outputStream=new DataOutputStream(clientSocket.getOutputStream());
+		outputStream.writeUTF(text1);
+		clientSocket.close();
+		}
+	}
+	catch(IOException ioe){
+		if(serverSocket!=null)
+		serverSocket.close();
+		ioe.printStackTrace();
+	}
+	}
+}
