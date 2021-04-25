caf12697568eb051467558a35a2e2c8e5f2be35c
Angel <qifang_2012@outlook.com>
2021-04-25 14:53:55 +0800

    Initial Commit

---
+++ Lab6/src/TemperatureRMI/ClientTemperatureRMI.java
@@ -0,0 +1,44 @@
+package TemperatureRMI;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import ftmk.rmi.sensor.TemperatureSensor;
+
+/**
+ * This class represent the client-side of RMI application
+ * 
+ */
+public class ClientTemperatureRMI {
+
+	public static void main(String[] args) {
+		
+		
+		try {
+			
+			// Get registry
+			Registry rmiRegistry = LocateRegistry.getRegistry();
+			
+			// Look-up for the remote object
+			TemperatureSensor remoteSensorJasin = (TemperatureSensor) rmiRegistry.lookup("SensorJasin");
+			
+			// Invoke method from the remote object
+			int currentTemperature = remoteSensorJasin.getTemperature();
+			
+			System.out.println("Current temperature in Jasin is " + currentTemperature + " Celcius");
+			
+			
+					
+			
+		} catch (RemoteException | NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
